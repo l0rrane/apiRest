df37b2a62b55b8a5e943d3e72ad1a5556e24fd12
l0rrane <gabrielalorraneespin@gmail.com>
2026-05-15 09:34:23 -0300

    metodo post put e delete

--- src/server.js
+++ src/server.js
@@ -30,6 +30,99 @@ app.get('/pessoas', async (req, res) => {
   }
 });
 
+
+app.post('/pessoas', async (req, res) => {
+  try {
+    const { nome, RA } = req.body;
+
+    if (!nome || !RA) {
+      return res.status(400).json({
+        mensagem: 'Os campos nome e curso sao obrigatorios.',
+      });
+    }
+
+    const novaPessoa = await Pessoa.create({ nome, RA });
+
+    res.status(201).json(novaPessoa);
+  } catch (error) {
+    res.status(500).json({
+      mensagem: 'Erro ao cadastrar pessoa.',
+      erro: error.message,
+    });
+  }
+});
+
+
+app.put('/pessoas', async (req, res) => {
+  try {
+
+    const { _id, nome, RA } = req.body;
+
+    if (!_id || !nome || !RA) {
+      return res.status(400).json({
+        mensagem: 'Os campos _id, nome e RA sao obrigatorios.',
+      });
+    }
+
+    const pessoa = await Pessoa.findById(_id);
+
+    if (!pessoa) {
+      return res.status(404).json({
+        mensagem: 'Pessoa nao encontrada.',
+      });
+    }
+
+    pessoa.nome = nome;
+    pessoa.RA = RA;
+
+    await pessoa.save();
+
+    res.status(200).json(pessoa);
+
+  } catch (error) {
+    res.status(500).json({
+      mensagem: 'Erro ao atualizar pessoa.',
+      erro: error.message,
+    });
+  }
+});
+
+
+
+app.delete('/pessoas', async (req, res) => {
+  try {
+
+    const { _id } = req.body;
+
+    if (!_id) {
+      return res.status(400).json({
+        mensagem: 'O campo _id é obrigatorio.',
+      });
+    }
+
+    const pessoa = await Pessoa.findById(_id);
+
+    if (!pessoa) {
+      return res.status(404).json({
+        mensagem: 'Pessoa nao encontrada.',
+      });
+    }
+
+    await pessoa.deleteOne();
+
+    res.status(200).json({
+      mensagem: 'Pessoa removida com sucesso.',
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      mensagem: 'Erro ao remover pessoa.',
+      erro: error.message,
+    });
+  }
+});
+
+
 async function startServer() {
   try {
     await connectDatabase();
